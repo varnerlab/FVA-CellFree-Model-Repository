9da5f9ba7b4ae4ebf3b4c0fa44bca80cc8f56d9a
nick <nhorvathwv@gmail.com>
2017-09-14 14:31:26 -0400

    Sep14_2

--- Solve.jl
+++
@@ -1,7 +0,0 @@
-include("Include.jl")
-
-# load the data dictionary -
-data_dictionary = DataDictionary(0,0,0)
-
-# solve the lp problem -
-(objective_value, flux_array, dual_array, uptake_array, exit_flag) = FluxDriver(data_dictionary)
